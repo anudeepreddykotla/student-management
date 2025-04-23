9a2c9a6167978e15ae82b4826a680c267ee8efa0
anudeepreddykotla <anudeepreddy.kotla@gmail.com>
2025-04-23 10:12:42 +0530

    Update students.js

--- frontend/src/api/students.js
+++ frontend/src/api/students.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/students';
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const getStudents = () => axios.get(BASE_URL);
 export const getStudentById = (id) => axios.get(`${BASE_URL}/${id}`);

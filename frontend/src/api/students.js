ed911f289cd22fae2e05890e296021fc545318b9
anudeepreddykotla <anudeepreddy.kotla@gmail.com>
2025-04-23 10:46:00 +0530

    Update students.js

--- frontend/src/api/students.js
+++ frontend/src/api/students.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-export const getStudents = () => axios.get(BASE_URL);
-export const getStudentById = (id) => axios.get(`${BASE_URL}/${id}`);
-export const addStudent = (student) => axios.post(BASE_URL, student);
-export const updateStudent = (id, student) => axios.put(`${BASE_URL}/${id}`, student);
-export const deleteStudent = (id) => axios.delete(`${BASE_URL}/${id}`);
+export const getStudents = () => axios.get(`${BASE_URL}/students`);
+export const getStudentById = (id) => axios.get(`${BASE_URL}/students/${id}`);
+export const addStudent = (student) => axios.post(`${BASE_URL}/students`, student);
+export const updateStudent = (id, student) => axios.put(`${BASE_URL}/students/${id}`, student);
+export const deleteStudent = (id) => axios.delete(`${BASE_URL}/students/${id}`);
